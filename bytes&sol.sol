3e8c39bbb330330037128c89051950abad17ab0f
Linus Shyu <83543818+Linus-Shyu@users.noreply.github.com>
2023-06-16 22:56:24 +0800

    Create bytes&sol.sol

---
+++ bytes&sol.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Bytes_Check {
+    bytes2 public display = 'XX';
+
+    bytes public display_2 = "Please follow Linus!!!";
+
+    uint public check = display_2.length;
+}
